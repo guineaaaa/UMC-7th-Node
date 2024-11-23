9e3588f856eeb046e0d15c729335698f0cb284f8
guineaaaa <165776804+guineaaaa@users.noreply.github.com>
2024-11-23 19:22:06 +0900

    ✨ 카카오 소셜 로그인 구현

--- src/auth.config.js
+++ src/auth.config.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { Strategy as KakaoStrategy } from "passport-kakao";
 import { prisma } from "./db.config.js";
 
 dotenv.config();
@@ -25,6 +26,49 @@ export const googleStrategy = new GoogleStrategy(
  * 그리고 사용자가 존재하지 않으면 기본 값과 함께 사용자 정보를 자동으로 생성하는 함수이다. */
 const googleVerify = async (profile) => {
   const email = profile.emails?.[0]?.value;
+  if (!email) {
+    throw new Error(`profile.email was not found: ${profile}`);
+  }
+
+  const member = await prisma.member.findFirst({ where: { email } });
+  if (member !== null) {
+    return { id: member.id, email: member.email, name: member.name };
+  }
+
+  const created = await prisma.member.create({
+    data: {
+      email,
+      name: profile.displayName,
+      gender: "추후 수정",
+      birth: "추후 수정",
+      address: "추후 수정",
+      spec_address: "추후 수정",
+      phone_num: "추후 수정",
+      age: 1,
+    },
+  });
+
+  return { id: created.id, email: created.email, name: created.name };
+};
+
+export const kakaoStrategy = new KakaoStrategy(
+  {
+    clientID: process.env.PASSPORT_KAKAO_CLIENT_ID,
+    clientSecret: process.env.PASSPORT_KAKAO_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/oauth2/callback/kakao",
+    scope: ["account_email"],
+    state: true,
+  },
+  (accessToken, refreshToken, profile, cb) => {
+    return kakaoVerify(profile)
+      .then((user) => cb(null, user))
+      .catch((err) => cb(err));
+  }
+);
+
+const kakaoVerify = async (profile) => {
+  // const email = profile.emails?.[0]?.value;
+  const email = profile._json.kakao_account.email;
   if (!email) {
     throw new Error(`profile.email was not found: ${profile}`);
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { PrismaSessionStore } from "@quixo3/prisma-session-store";
 import session from "express-session";
 import passport from "passport";
-import { googleStrategy } from "./auth.config.js";
+import { googleStrategy, kakaoStrategy } from "./auth.config.js";
 import { prisma } from "./db.config.js";
 
 import cors from "cors";
@@ -24,6 +24,7 @@ import {
 } from "./controllers/memberMission.controller.js";
 
 passport.use(googleStrategy); // passport 라이브러리에 정의한 로그인 방식 등록
+passport.use(kakaoStrategy);
 passport.serializeUser((user, done) => done(null, user)); // 세션에 사용자 정보를 저장할 때, 세션 정보를 가져올 때 사용
 passport.deserializeUser((user, done) => done(null, user));
 
@@ -90,12 +91,6 @@ app.get("/openapi.json", async (req, res, next) => {
   res.json(result ? result.data : null);
 });
 
-// Passport 사용하기 위한 루트
-// /oauth2/login/google: 로 접속하면 자동으로 Google 로그인 주소로 이동하여 사용자가 Google 로그인을 할 수 있도록 함
-// oauth2/callback/google: Google 로그인이 성공하면 자동으로 되돌아 오는 주소이다.
-// 여기에는 쿼리 파라미터로 전달된 code 값을 통해 Google API를 호출하여 사용자의 프로필 정도를 조회하고,
-// 이것을 Session DB에 저장한다.
-
 app.use(cors()); // cors 방식 허용
 app.use(express.static("public")); // 정적 파일 접근
 app.use(express.json()); // request의 본문을 json으로 해석할 수 있도록 함 (JSON 형태의 요청 body를 파싱하기 위함)
@@ -122,6 +117,16 @@ app.get(
   "/oauth2/callback/google",
   passport.authenticate("google", {
     failureRedirect: "/oauth2/login/google",
+    failureMessage: true,
+  }),
+  (req, res) => res.redirect("/")
+);
+
+app.get("/oauth2/login/kakao", passport.authenticate("kakao"));
+app.get(
+  "/oauth2/callback/kakao",
+  passport.authenticate("kakao", {
+    failureRedirect: "/oauth2/login/kakao",
     failureMessage: true,
   }),
   (req, res) => res.redirect("/")
